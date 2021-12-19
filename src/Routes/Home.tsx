fad045e5d424566a253c4ef4ceae670c69833d17
Jiyeon <yeeoni_@naver.com>
2021-12-19 18:10:17 +0900

    [Feat] 라이브러리 설치와 프로젝트 셋팅

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Wetube</title>
+      </Helmet>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
