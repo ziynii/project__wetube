d19f5cb6961961b27a57a5e41a10dffc6c5eab98
Jiyeon <yeeoni_@naver.com>
2021-12-20 20:59:14 +0900

    [Feat] 동영상 검색시 VidoeListTitle를 검색한 단어에 맞게 변경

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,33 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import styled from 'styled-components';
 import { IVideo } from '../App';
-import { motion } from 'framer-motion';
 import VideoItem from '../Components/VideoItem';
 
 const Wrapper = styled.div`
   padding: 20px 60px;
 `;
 
-const Vidoelist = styled(motion.ul)`
+const Vidoelist = styled.ul`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  gap: 10px;
+  column-gap: 10px;
+  row-gap: 16px;
 `;
 
 const VidoeListTitle = styled.h3`
-  color: ${(props) => props.theme.accentColor};
+  color: ${(props) => props.theme.subTextColor};
   font-size: 16px;
   font-weight: 400;
   margin-bottom: 12px;
-	text-transform: uppercase;
+  text-transform: uppercase;
+  span {
+		color : ${(props) => props.theme.mainTextColor};
+    font-weight: bold;
+  }
 `;
 
 export interface IVideosProps {
   videos: IVideo[];
   openNav: boolean;
+  isQuery: string | undefined;
 }
 
-const Home = ({ videos, openNav }: IVideosProps) => {
+const Home = ({ videos, openNav, isQuery }: IVideosProps) => {
   return (
     <>
       <HelmetProvider>
@@ -37,7 +42,13 @@ const Home = ({ videos, openNav }: IVideosProps) => {
         </Helmet>
       </HelmetProvider>
       <Wrapper>
-        <VidoeListTitle>most popular</VidoeListTitle>
+        {isQuery ? (
+          <VidoeListTitle>
+            <span>'{isQuery}'</span> 의 검색결과 입니다.
+          </VidoeListTitle>
+        ) : (
+          <VidoeListTitle>most popular</VidoeListTitle>
+        )}
         <Vidoelist>
           {videos.map((video) => (
             <VideoItem video={video} key={video.id} openNav={openNav} />
