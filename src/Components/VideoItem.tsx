59eebdf1fb0b7bce5b241c3ade7994dd926c3e12
Jiyeon <yeeoni_@naver.com>
2022-05-18 21:24:12 +0900

    💄 Change width from 270px to 100% in Video

--- src/Components/VideoItem.tsx
+++ src/Components/VideoItem.tsx
@@ -7,9 +7,9 @@ interface IVideoListProps {
   openNav: boolean;
 }
 
-const Video = styled.li<{ openNav: boolean }>`
+const Video = styled.li`
   cursor: pointer;
-  width: ${(props) => (props.openNav ? '270px' : '100%')};
+  width: 100%;
   img {
     border-radius: 4px;
     width: 100%;
@@ -28,18 +28,23 @@ const VideoTitle = styled.h5`
   color: ${(props) => props.theme.mainTextColor};
   font-weight: 400;
   font-size: 16px;
-  margin-top: 4px;
+  margin-top: 12px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
 
 const ChannelTitle = styled.p`
   color: ${(props) => props.theme.subTextColor};
   font-size: 14px;
-  margin-top: 2px;
+  margin-top: 8px;
 `;
 
 const VideoItem = ({ video, openNav }: IVideoListProps) => {
   return (
-    <Video openNav={openNav}>
+    <Video>
       <img src={video.snippet.thumbnails.medium.url} alt="비디오 썸네일" />
       <VideoTitle>{video.snippet.title}</VideoTitle>
       <ChannelTitle>{video.snippet.channelTitle}</ChannelTitle>
