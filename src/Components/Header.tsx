e316765236bb01c3229a3691bcd8c1f639e4f539
Jiyeon <yeeoni_@naver.com>
2022-05-23 22:12:26 +0900

    💄 Change style when desktop and mobile size

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import logo from '../image/logo.png';
 import { Link } from 'react-router-dom';
 import SearchInput from './SearchInput';
+import { useState } from 'react';
+import Overlay from './Overlay';
 
 const Wrapper = styled.header`
   width: 100%;
@@ -15,8 +17,36 @@ const Wrapper = styled.header`
   z-index: 500;
   background-color: white;
 
-  @media all and (max-width: 1023px) {
+  @media all and (max-width: 767px) {
     padding: 20px 20px;
+  }
+`;
+
+const DSearchWrap = styled.div`
+  width: 100%;
+
+  @media all and (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const MSearchWrap = styled.div`
+  display: none;
+  width: 100%;
+
+  form {
+    position: absolute;
+    top: 160px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  input {
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+
+  @media all and (max-width: 767px) {
+    display: block;
   }
 `;
 
@@ -24,7 +54,7 @@ const HeaderLeft = styled.div`
   display: flex;
   flex-basis: 20%;
   align-items: center;
-  margin-top: 4px;
+
   button {
     outline: none;
     border: 0;
@@ -33,6 +63,7 @@ const HeaderLeft = styled.div`
     color: ${(props) => props.theme.subTextColor};
     transition: color 200ms ease;
     cursor: pointer;
+
     &:hover {
       color: ${(props) => props.theme.mainTextColor};
     }
@@ -47,23 +78,32 @@ const HeaderLeft = styled.div`
       margin-left: 12px;
       width: 75px;
     }
+
     button {
       font-size: 14px;
+    }
+  }
+
+  @media all and (max-width: 767px) {
+    img {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      margin-left: 0;
     }
   }
 `;
 
 const HeaderRight = styled.div`
-  flex-basis: 15%;
-  display: flex;
-  justify-content: flex-end;
-  margin-right: 12px;
+  flex-basis: 20%;
   svg {
     font-size: 18px;
     margin-right: 12px;
     color: ${(props) => props.theme.subTextColor};
     transition: color 200ms ease;
     cursor: pointer;
+
     &:hover {
       color: ${(props) => props.theme.mainTextColor};
     }
@@ -81,13 +121,43 @@ const HeaderRight = styled.div`
   }
 `;
 
+const DIcon = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  @media all and (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const MIcon = styled.div`
+  display: none;
+  text-align: right;
+
+  @media all and (max-width: 767px) {
+    display: block;
+  }
+`;
+
+const IconButton = styled.button`
+  outline: none;
+  border: none;
+  background-color: transparent;
+`;
+
 const Login = styled.button`
-  flex-basis: 5%;
+  width: 96px;
   padding: 10px 0;
   border-radius: 4px;
   border: none;
   background-color: ${(props) => props.theme.accentColor};
+  border: 1px solid ${(props) => props.theme.accentColor};
   color: white;
+
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+    background-color: ${(props) => props.theme.bgColor};
+    transition: all 200ms ease-in-out;
+  }
 
   @media all and (max-width: 1023px) {
     display: none;
@@ -109,6 +179,8 @@ const Header = ({
   setKeyword,
   popular,
 }: IHeaderProps) => {
+  const [openSearch, setOpenSearch] = useState(false);
+
   return (
     <>
       <Wrapper>
@@ -120,15 +192,45 @@ const Header = ({
             <img src={logo} alt="youtube 로고" />
           </Link>
         </HeaderLeft>
-        <SearchInput
-          setKeyword={setKeyword}
-          onSearch={onSearch}
-          keyword={keyword}
-        />
+
+        <DSearchWrap>
+          <SearchInput
+            setKeyword={setKeyword}
+            onSearch={onSearch}
+            keyword={keyword}
+            setOpenSearch={setOpenSearch}
+          />
+        </DSearchWrap>
+        <MSearchWrap>
+          {openSearch ? (
+            <>
+              <SearchInput
+                setKeyword={setKeyword}
+                onSearch={onSearch}
+                keyword={keyword}
+                setOpenSearch={setOpenSearch}
+              />
+              <Overlay setOpenSearch={setOpenSearch} />
+            </>
+          ) : null}
+        </MSearchWrap>
         <HeaderRight>
-          <i className="far fa-plus-square"></i>
-          <i className="far fa-heart"></i>
-          <i className="far fa-bell"></i>
+          <DIcon>
+            <IconButton>
+              <i className="far fa-plus-square"></i>
+            </IconButton>
+            <IconButton>
+              <i className="far fa-heart"></i>
+            </IconButton>
+            <IconButton>
+              <i className="far fa-bell"></i>
+            </IconButton>
+          </DIcon>
+          <MIcon>
+            <IconButton onClick={() => setOpenSearch(true)}>
+              <i className="fas fa-search"></i>
+            </IconButton>
+          </MIcon>
         </HeaderRight>
         <Login>Sign in</Login>
       </Wrapper>
